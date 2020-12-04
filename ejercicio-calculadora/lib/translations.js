0905a54484cf9df55f0b19b38a522d798f9f5f8e
Berto Yáñez <berto@hey.com>
2020-12-04 19:15:02 +0100

    Ejercicio calculadora

---
+++ ejercicio-calculadora/lib/translations.js
@@ -0,0 +1,18 @@
+module.exports = {
+  suma: {
+    es: "sumar",
+    en: "adding",
+  },
+  resta: {
+    es: "restar",
+    en: "substracting",
+  },
+  multiplica: {
+    es: "multiplicar",
+    en: "multipliying",
+  },
+  divide: {
+    es: "dividir",
+    en: "dividing",
+  },
+};
